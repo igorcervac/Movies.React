405239c8bdd282f260a08661602b61a5d4f14da7
igorcervac <igor.cervac@outlook.com>
2025-01-29 16:20:04 -0500

    Implement fetch movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+type Movie = {
+  imdbID: string,
+  Type: string,
+  Title: string,
+  Year: string,
+  Poster: string
+}
+
 function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [search, setSearch] = useState<string>('Star Wars');
+
+    useEffect(() => {
+      const getMovies = async () => {
+        const response = await fetch(`http://www.omdbapi.com?s=${search}&apikey=ebd94699`);
+        const json = await response.json();
+        setMovies(json.Search);
+      }
+
+      getMovies();
+    }, [search]);
+
   return (
   <div>
       <h1>Movies</h1>
+
+      <div className='movies'>
+        {
+          movies.map(x => (
+          <div key={x.imdbID} className='movie'>
+            <h3>{x.Title}</h3>
+            <img src={x.Poster} alt={x.Title}/>
+          </div>))
+        }
+      </div>
+
     </div>
   );
 }
