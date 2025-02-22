4d5496afed2ec9947ce76a6e38760b4bb3f2d55d
igorcervac <igor.cervac@outlook.com>
2025-02-22 00:00:37 -0500

    Implement routing. Implement About component

--- src/App.tsx
+++ src/App.tsx
@@ -1,98 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import Movie from './Movie';
+import React, {  } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import About from './components/About';
 import Movies from './components/Movies';
-import AddFavourite from './components/AddFavourite';
-import RemoveFavourite from './components/RemoveFavourite';
-import SearchBox from './components/SearchBox';
-import favouriteMovieService from './services/ApiFavouriteMovieService';
-import FavouriteMovies from './components/FavouriteMovies';
 
-function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [favouriteMovies, setFavouriteMovies] = useState<Movie[]>([]);
-
-  const [search, setSearch] = useState<string>('Star Wars');
-
-    useEffect(() => {
-      const getMovies = async () => {
-        const response = await fetch(`https://www.omdbapi.com?s=${search}&apikey=ebd94699`);
-        const json = await response.json();
-        const searchedMovies = json.Search ?? [];
-        setMovies(searchedMovies.map(
-          (x: any) => 
-          (
-            {
-              imdbID: x.imdbID, 
-              title: x.Title, 
-              poster: x.Poster, 
-              year: x.Year, 
-              type: x.Type
-            }
-          ) as Movie
-        ));
-      };
-
-      getMovies();
-    }, [search]);
-
-    useEffect(() => {
-      const getFavouriteMovies = async () => {
-        const json = await favouriteMovieService.getAll();
-        setFavouriteMovies(json);
-      };
-
-      getFavouriteMovies();
-      
-    },[]);
-
-    const addToFavourites = async (movie: Movie)=> {
-        setFavouriteMovies([...favouriteMovies, movie]);
-        // setMovies(movies.filter(x => !favouriteMovies.includes(x)));
-        await favouriteMovieService.add(movie);
-    }
-
-    const removeFromFavourites = async (movie: Movie) => {
-      setFavouriteMovies(favouriteMovies.filter(x => x.id !== movie.id));
-      // setMovies(movies.filter(x => !favouriteMovies.includes(x)));
-      await favouriteMovieService.remove(movie.id);
-    }
-
-    const searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-      console.log(e.target.value);
-        setSearch(e.target.value);
-    }
-
+function App() { 
   return (
-  <div>
+    <BrowserRouter>
       <header>
         <h1>Movies</h1>
-        <nav>
-          <a href='/'>Home</a>
-          <a href='/about'>About</a>
-        </nav>
+          <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+          </nav>
       </header>
       <main>
-        <div className='movies-header'>
-            <h2>All movies</h2>
-            <SearchBox searchHandler={searchHandler} searchValue={search}></SearchBox>
-        </div>
-        <div className='container-fluid movies'>         
-          <div className='row'>
-            <Movies movies={movies} favouriteComponent={AddFavourite} favouriteActionHandler={addToFavourites}></Movies>
-          </div>
-        </div>
-
-        <h2>Favourite  movies</h2>
-        <div className='container-fluid movies'>
-          <div className='row'>
-            <FavouriteMovies movies={favouriteMovies} favouriteComponent={RemoveFavourite} favouriteActionHandler={removeFromFavourites}></FavouriteMovies>
-          </div>
-        </div>
+        <Routes>
+          <Route path='/' Component={Movies}></Route>
+          <Route path='/about' Component={About}></Route>
+        </Routes>
       </main>
-
-    </div>
+    </BrowserRouter>       
   );
 }
 
--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,23 +1,86 @@
-import React from 'react';
-import Movie from '../Movie'
+import React, { useEffect, useState } from "react";
+import AllMovies from "./AllMovies";
+import FavouriteMovies from "./FavouriteMovies";
+import SearchBox from "./SearchBox";
+import Movie from "../Movie";
+import favouriteMovieService from "../services/ApiFavouriteMovieService";
+import AddFavourite from "./AddFavourite";
+import RemoveFavourite from "./RemoveFavourite";
 
-const Movies = (props: any) => {
+const Movies = () => {
 
-    const FavouriteComponent = props.favouriteComponent;
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [favouriteMovies, setFavouriteMovies] = useState<Movie[]>([]);
+  
+    const [search, setSearch] = useState<string>('Star Wars');
+  
+      useEffect(() => {
+        const getMovies = async () => {
+          const response = await fetch(`https://www.omdbapi.com?s=${search}&apikey=ebd94699`);
+          const json = await response.json();
+          const searchedMovies = json.Search ?? [];
+          setMovies(searchedMovies.map(
+            (x: any) => 
+            (
+              {
+                imdbID: x.imdbID, 
+                title: x.Title, 
+                poster: x.Poster, 
+                year: x.Year, 
+                type: x.Type
+              }
+            ) as Movie
+          ));
+        };
+  
+        getMovies();
+      }, [search]);
+  
+      useEffect(() => {
+        const getFavouriteMovies = async () => {
+          const json = await favouriteMovieService.getAll();
+          setFavouriteMovies(json);
+        };
+  
+        getFavouriteMovies();
+        
+      },[]);
+  
+      const addToFavourites = async (movie: Movie)=> {
+          setFavouriteMovies([...favouriteMovies, movie]);
+          // setMovies(movies.filter(x => !favouriteMovies.includes(x)));
+          await favouriteMovieService.add(movie);
+      }
+  
+      const removeFromFavourites = async (movie: Movie) => {
+        setFavouriteMovies(favouriteMovies.filter(x => x.id !== movie.id));
+        // setMovies(movies.filter(x => !favouriteMovies.includes(x)));
+        await favouriteMovieService.remove(movie.id);
+      }
+  
+      const searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+          setSearch(e.target.value);
+      }
 
-    return (
-        <>
-            {
-                props.movies.map((x: Movie) => 
-                (
-                    <div key={x.imdbID} className='image-container d-flex justify-content-start'>
-                        <img src={x.poster} alt={x.title}></img>                        
-                        <FavouriteComponent favouriteActionHandler={async () => await props.favouriteActionHandler(x)}/>
-                    </div>
-                ))
-            }
-        </>
-    )
+    return (<>
+        <div className='movies-header'>
+                <h2>All movies</h2>
+                <SearchBox searchHandler={searchHandler} searchValue={search}></SearchBox>
+            </div>
+            <div className='container-fluid movies'>         
+            <div className='row'>
+                <AllMovies movies={movies} favouriteComponent={AddFavourite} favouriteActionHandler={addToFavourites}></AllMovies>
+            </div>
+            </div>
+
+            <h2>Favourite  movies</h2>
+            <div className='container-fluid movies'>
+            <div className='row'>
+                <FavouriteMovies movies={favouriteMovies} favouriteComponent={RemoveFavourite} favouriteActionHandler={removeFromFavourites}></FavouriteMovies>
+            </div>
+            </div>
+        </>);
 }
 
 export default Movies;
